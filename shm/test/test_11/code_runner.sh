49a1bce4c81eb078cd510373395d87f6824d8ce0
abhinayh56 <kumarabhinay53@gmail.com>
2025-05-27 12:27:24 +0530

    asdfasdf

--- shm/test/test_11/code_runner.sh
+++ shm/test/test_11/code_runner.sh
@@ -1,5 +1,6 @@
 g++ -std=c++17 writer.cpp -o writer -lpthread
 g++ -std=c++17 reader.cpp -o reader -lpthread
 
-# ./writer    # in one terminal
+# ./writer 1    # in one terminal
+# ./writer 2    # in another terminal
 # ./reader    # in another (run multiple if needed)
